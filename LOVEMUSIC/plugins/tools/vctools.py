8b5fc6a8a2c63d14a1b4aa9cc04ee3bd1df9077f
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-12 02:13:10 +0530

    Update vctools.py

--- LOVEMUSIC/plugins/tools/vctools.py
+++ LOVEMUSIC/plugins/tools/vctools.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from VIPMUSIC import app
+from LOVEMUSIC import app
 from config import OWNER_ID
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
