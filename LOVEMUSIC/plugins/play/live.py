bd751283df9744bc8c98eb637ca39e15dd6b63da
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-12 01:26:44 +0530

    Update live.py

--- LOVEMUSIC/plugins/play/live.py
+++ LOVEMUSIC/plugins/play/live.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from VIPMUSIC import YouTube, app
-from VIPMUSIC.utils.channelplay import get_channeplayCB
-from VIPMUSIC.utils.decorators.language import languageCB
-from VIPMUSIC.utils.stream.stream import stream
+from LOVEMUSIC import YouTube, app
+from LOVEMUSIC.utils.channelplay import get_channeplayCB
+from LOVEMUSIC.utils.decorators.language import languageCB
+from lOVEMUSIC.utils.stream.stream import stream
 from config import BANNED_USERS
 
 
