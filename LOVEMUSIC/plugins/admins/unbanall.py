bce4113b5126002bd8fd2df4b2e7c2ab78a92d74
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-11 23:28:42 +0530

    Update unbanall.py

--- LOVEMUSIC/plugins/admins/unbanall.py
+++ LOVEMUSIC/plugins/admins/unbanall.py
@@ -1,8 +1,8 @@
-from VIPMUSIC import app
+from LOVEMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from VIPMUSIC.utils.vip_ban import admin_filter
+from LOVEMUSIC.utils.LOVE_ban import admin_filter
 
 BOT_ID = app.id
 
