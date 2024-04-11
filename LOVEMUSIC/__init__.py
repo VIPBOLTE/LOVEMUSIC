ba297ffe92599c48472b91e4a79d4d1a08a4123e
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-11 21:33:19 +0530

    Add files via upload

---
+++ LOVEMUSIC/__init__.py
@@ -0,0 +1,32 @@
+from VIPMUSIC.core.bot import VIP
+from VIPMUSIC.core.dir import dirr
+from VIPMUSIC.core.git import git
+from VIPMUSIC.core.userbot import Userbot
+from VIPMUSIC.misc import dbb, heroku
+from telethon import TelegramClient
+from config import API_ID, API_HASH
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = VIP()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+telethn = TelegramClient("VIPMUSIC", API_ID, API_HASH)
---
+++ LOVEMUSIC/__main__.py
@@ -0,0 +1,59 @@
+import asyncio
+import importlib
+from sys import argv
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from VIPMUSIC import LOGGER, app, userbot
+from VIPMUSIC.core.call import VIP
+from VIPMUSIC.misc import sudo
+from VIPMUSIC.plugins import ALL_MODULES
+from VIPMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+from VIPMUSIC import telethn
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧🤬")
+        
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            if user_id not in BANNED_USERS:
+                BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("VIPMUSIC.plugins" + all_module)
+    LOGGER("VIPMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    await userbot.start()
+    await VIP.start()
+    await VIP.decorators()
+    LOGGER("VIPMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝"
+    )
+    await idle()
+    if len(argv) not in (1, 3, 4):
+        await telethn.disconnect()
+    else:
+        await telethn.run_until_disconnected()
+                
+    await app.stop()
+    await userbot.stop()
+    LOGGER("VIPMUSIC").info("                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    
+
+if __name__ == "__main__":
+    telethn.start(bot_token=config.BOT_TOKEN)
+    asyncio.get_event_loop().run_until_complete(init())
