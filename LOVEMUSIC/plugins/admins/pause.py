e43204dc07a8a787e9cefe359e62432a82d77e4f
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-11 23:43:34 +0530

    Update pause.py

--- LOVEMUSIC/plugins/admins/pause.py
+++ LOVEMUSIC/plugins/admins/pause.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from VIPMUSIC import app
-from VIPMUSIC.core.call import VIP
-from VIPMUSIC.utils.database import is_music_playing, music_off
-from VIPMUSIC.utils.decorators import AdminRightsCheck
-from VIPMUSIC.utils.inline import close_markup
+from LOVEMUSIC import app
+from LOVEMUSIC.core.call import VIP
+from LOVEMUSIC.utils.database import is_music_playing, music_off
+from LOVEMUSIC.utils.decorators import AdminRightsCheck
+from LOVEMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 @app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
