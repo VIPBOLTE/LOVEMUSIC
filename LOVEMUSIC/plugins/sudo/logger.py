c6fc64f360c3b35a1a76ee55bdf073377699107b
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-12 01:42:25 +0530

    Update logger.py

--- LOVEMUSIC/plugins/sudo/logger.py
+++ LOVEMUSIC/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from VIPMUSIC import app
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import add_off, add_on
-from VIPMUSIC.utils.decorators.language import language
+from LOVEMUSIC import app
+from LOVEMUSIC.misc import SUDOERS
+from LOVEMUSIC.utils.database import add_off, add_on
+from LOVEMUSIC.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
