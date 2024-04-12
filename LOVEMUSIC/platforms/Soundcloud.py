9ba5068678eb195563ade0e67b0858abf1752052
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-12 09:27:15 +0530

    Update Soundcloud.py

--- LOVEMUSIC/platforms/Soundcloud.py
+++ LOVEMUSIC/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from VIPMUSIC.utils.formatters import seconds_to_min
+from LOVEMUSIC.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
