8abbd68b5c2f337163ae25ec14bf291f57736624
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-12 09:24:57 +0530

    Update Youtube.py

--- LOVEMUSIC/platforms/Youtube.py
+++ LOVEMUSIC/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from VIPMUSIC.utils.database import is_on_off
-from VIPMUSIC.utils.formatters import time_to_seconds
+from LOVEMUSIC.utils.database import is_on_off
+from LOVEMUSIC.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
