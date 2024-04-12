08eb10e0f221d7d65760d7bd8561610b60f99466
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-12 11:08:19 +0530

    Update extraction.py

--- LOVEMUSIC/utils/extraction.py
+++ LOVEMUSIC/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from VIPMUSIC import app
+from LOVEMUSIC import app
 
 
 async def extract_user(m: Message) -> User:
