074708808bc773e527558403c30ff839a727cb5d
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-12 11:41:06 +0530

    Update play.py

--- LOVEMUSIC/utils/inline/play.py
+++ LOVEMUSIC/utils/inline/play.py
@@ -2,9 +2,9 @@ import math
 from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
-from VIPMUSIC.utils.formatters import time_to_seconds
+from LOVEMUSIC.utils.formatters import time_to_seconds
 
-from VIPMUSIC import app
+from LOVEMUSIC import app
 
 def track_markup(_, user_id, channel, fplay):
     buttons = [
