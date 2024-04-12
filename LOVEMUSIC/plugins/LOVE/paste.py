d57c1a4e84fafefb7c1a2f571f2c84e18cf6d54f
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-12 09:19:08 +0530

    Update paste.py

--- LOVEMUSIC/plugins/LOVE/paste.py
+++ LOVEMUSIC/plugins/LOVE/paste.py
@@ -1,6 +1,6 @@
 from asyncio import get_running_loop, sleep, TimeoutError
 from functools import partial
-from VIPMUSIC import app
+from LOVEMUSIC import app
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiohttp import ClientSession
