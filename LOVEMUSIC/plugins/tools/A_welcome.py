d6c0c8b3789ec5391eb19b866b6b2523fd354cec
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-13 12:16:40 +0530

    Update A_welcome.py

--- LOVEMUSIC/plugins/tools/A_welcome.py
+++ LOVEMUSIC/plugins/tools/A_welcome.py
@@ -20,7 +20,7 @@ from pyrogram.enums import ParseMode
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from LOVEMUSIC.utils.vip_ban import admin_filter
+from LOVEMUSIC.utils.LOVE_ban import admin_filter
 import os
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
