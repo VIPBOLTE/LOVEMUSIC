75398570a6bdbccb517b0912890acebea68d2f1d
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-13 11:05:03 +0530

    Update watcher.py

--- LOVEMUSIC/plugins/misc/watcher.py
+++ LOVEMUSIC/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from LOVEMUSIC import app
-from LOVEMUSIC.core.call import VIP
+from LOVEMUSIC.core.call import LOVE
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await VIP.stop_stream_force(message.chat.id)
+    await LOVE.stop_stream_force(message.chat.id)
