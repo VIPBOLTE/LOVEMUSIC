d76079113f23f565438e5efcd23e13ed63c8cca3
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-13 11:18:01 +0530

    Update play.py

--- LOVEMUSIC/plugins/play/play.py
+++ LOVEMUSIC/plugins/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 from VIPMUSIC.utils.database import get_assistant
 import config
-from VIPMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from VIPMUSIC.core.call import VIP
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils import seconds_to_min, time_to_seconds
-from VIPMUSIC.utils.channelplay import get_channeplayCB
-from VIPMUSIC.utils.decorators.language import languageCB
-from VIPMUSIC.utils.decorators.play import PlayWrapper
-from VIPMUSIC.utils.formatters import formats
-from VIPMUSIC.utils.inline import (
+from LOVEMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from LOVEMUSIC.core.call import LOVE
+from LOVEMUSIC.misc import SUDOERS
+from LOVEMUSIC.utils import seconds_to_min, time_to_seconds
+from LOVEMUSIC.utils.channelplay import get_channeplayCB
+from LOVEMUSIC.utils.decorators.language import languageCB
+from LOVEMUSIC.utils.decorators.play import PlayWrapper
+from LOVEMUSIC.utils.formatters import formats
+from LOVEMUSIC.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from VIPMUSIC.utils.database import (
+from LOVEMUSIC.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -29,10 +29,10 @@ from VIPMUSIC.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from VIPMUSIC.utils.logger import play_logs
+from LOVEMUSIC.utils.logger import play_logs
 from config import BANNED_USERS, lyrical
 from time import time
-from VIPMUSIC.utils.extraction import extract_user
+from LOVEMUSIC.utils.extraction import extract_user
 
 # Define a dictionary to track the last message timestamp for each user
 user_last_message_time = {}
@@ -567,7 +567,7 @@ async def play_music(client, CallbackQuery, _):
     return await mystic.delete()
 
 
-@app.on_callback_query(filters.regex("VIPmousAdmin") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("LOVEmousAdmin") & ~BANNED_USERS)
 async def VIPmous_check(client, CallbackQuery):
     try:
         await CallbackQuery.answer(
@@ -578,7 +578,7 @@ async def VIPmous_check(client, CallbackQuery):
         pass
 
 
-@app.on_callback_query(filters.regex("VIPPlaylists") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("LOVEPlaylists") & ~BANNED_USERS)
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
@@ -717,14 +717,14 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from VIPMUSIC import Carbon, YouTube, app
-from VIPMUSIC.core.call import VIP
-from VIPMUSIC.misc import db
-from VIPMUSIC.utils.database import add_active_video_chat, is_active_chat
-from VIPMUSIC.utils.exceptions import AssistantErr
-from VIPMUSIC.utils.inline import aq_markup, queuemarkup, close_markup, stream_markup, stream_markup2, panel_markup_4
-from VIPMUSIC.utils.pastebin import VIPBin
-from VIPMUSIC.utils.stream.queue import put_queue, put_queue_index
+from LOVEMUSIC import Carbon, YouTube, app
+from LOVEMUSIC.core.call import LOVE
+from LOVEMUSIC.misc import db
+from LOVEMUSIC.utils.database import add_active_video_chat, is_active_chat
+from LOVEMUSIC.utils.exceptions import AssistantErr
+from LOVEMUSIC.utils.inline import aq_markup, queuemarkup, close_markup, stream_markup, stream_markup2, panel_markup_4
+from LOVEMUSIC.utils.pastebin import LOVEBin
+from LOVEMUSIC.utils.stream.queue import put_queue, put_queue_index
 from youtubesearchpython.__future__ import VideosSearch
 
 
@@ -744,7 +744,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await VIP.force_stop_stream(chat_id)
+        await LOVE.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -791,7 +791,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await VIP.join_call(
+                await LOVE.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -828,7 +828,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await VIPBin(msg)
+            link = await LOVEBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -937,7 +937,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await VIP.join_call(chat_id, original_chat_id, file_path, video=None)
+            await LOVE.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -989,7 +989,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await VIP.join_call(chat_id, original_chat_id, file_path, video=status)
+            await LOVE.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -1045,7 +1045,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await VIP.join_call(
+            await LOVE.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -1103,7 +1103,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await VIP.join_call(
+            await LOVE.join_call(
                 chat_id,
                 original_chat_id,
                 link,
