941a8e76f05b90565f81d0a585bec8f5d444798f
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-13 05:50:02 +0530

    Update pause.py

--- LOVEMUSIC/plugins/admins/pause.py
+++ LOVEMUSIC/plugins/admins/pause.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from LOVEMUSIC import app
-from LOVEMUSIC.core.call import VIP
+from LOVEMUSIC.core.call import LOVE
 from LOVEMUSIC.utils.database import is_music_playing, music_off
 from LOVEMUSIC.utils.decorators import AdminRightsCheck
 from LOVEMUSIC.utils.inline import close_markup
@@ -13,7 +13,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await VIP.pause_stream(chat_id)
+    await LOVE.pause_stream(chat_id)
     
     buttons = [
         [
