ddec52647dba685b7892325e7de1f4ce2cd5bcff
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-13 08:58:31 +0530

    Update stop.py

--- LOVEMUSIC/plugins/admins/stop.py
+++ LOVEMUSIC/plugins/admins/stop.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from LOVEMUSIC import app
-from LOVEMUSIC.core.call import VIP
+from LOVEMUSIC.core.call import LOVE
 from LOVEMUSIC.utils.database import set_loop
 from LOVEMUSIC.utils.decorators import AdminRightsCheck
 from LOVEMUSIC.utils.inline import close_markup
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await VIP.stop_stream(chat_id)
+    await LOVE.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
