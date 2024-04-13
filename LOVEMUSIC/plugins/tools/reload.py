10c3fcea42ac74a53e3fceb652b566c1b258fb70
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-13 14:15:42 +0530

    Update reload.py

--- LOVEMUSIC/plugins/tools/reload.py
+++ LOVEMUSIC/plugins/tools/reload.py
@@ -13,7 +13,7 @@ from pyrogram import filters
 load_dotenv()
 
 from LOVEMUSIC import app
-from LOVEMUSIC.core.call import VIP
+from LOVEMUSIC.core.call import LOVE
 from LOVEMUSIC.misc import db
 from LOVEMUSIC.utils.database import get_assistant, get_authuser_names, get_cmode
 from LOVEMUSIC.utils.decorators import ActualAdminCB, AdminActual, language
@@ -66,7 +66,7 @@ async def restartbot(client, message: Message, _):
     await asyncio.sleep(1)
     try:
         db[message.chat.id] = []
-        await VIP.stop_stream_force(message.chat.id)
+        await LOVE.stop_stream_force(message.chat.id)
     except:
         pass
     userbot = await get_assistant(message.chat.id)
@@ -93,7 +93,7 @@ async def restartbot(client, message: Message, _):
             pass
         try:
             db[chat_id] = []
-            await VIP.stop_stream_force(chat_id)
+            await LOVE.stop_stream_force(chat_id)
         except:
             pass
     return await mystic.edit_text(_["reload_5"].format(app.mention))
