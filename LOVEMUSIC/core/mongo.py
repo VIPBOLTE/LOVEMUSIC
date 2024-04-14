bd066d2e90d4726fd08e9941d190bcd1f6c0e5d2
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-14 08:59:03 +0530

    Update mongo.py

--- LOVEMUSIC/core/mongo.py
+++ LOVEMUSIC/core/mongo.py
@@ -14,7 +14,7 @@ if config.MONGO_DB_URI is None:
         "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐕𝐈𝐏 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
     )
     temp_client = Client(
-        "VIPMUSIC",
+        "LOVEMUSIC",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
@@ -30,5 +30,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.VIPMUSIC
-    pymongodb = _mongo_sync_.VIPMUSIC
+    mongodb = _mongo_async_.LOVEMUSIC
+    pymongodb = _mongo_sync_.LOVEMUSIC
