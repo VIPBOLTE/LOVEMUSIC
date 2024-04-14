8bef26b8adc5ab5e0b6ea58ccbbb65f6e2a63ab6
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-14 09:09:48 +0530

    Update thumbnails.py

--- LOVEMUSIC/utils/thumbnails.py
+++ LOVEMUSIC/utils/thumbnails.py
@@ -84,8 +84,8 @@ async def get_thumb(videoid):
         #enhancer = ImageEnhance.Brightness(background)
         #background = enhancer.enhance(0.9)
         #draw = ImageDraw.Draw(background)
-        #arial = ImageFont.truetype("VIPMUSIC/assets/font2.ttf", 30)
-        #font = ImageFont.truetype("VIPMUSIC/assets/font.ttf", 30)
+        #arial = ImageFont.truetype("LOVEMUSIC/assets/font2.ttf", 30)
+        #font = ImageFont.truetype("LOVEMUSIC/assets/font.ttf", 30)
         # draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
         """
         draw.text(
