50a3ba5119c5ae8da72542a0a0ce5b88aca08d1b
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-14 09:06:02 +0530

    Update A_welcome.py

--- LOVEMUSIC/plugins/tools/A_welcome.py
+++ LOVEMUSIC/plugins/tools/A_welcome.py
@@ -92,13 +92,13 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("VIPMUSIC/assets/wel2.png")
+    background = Image.open("LOVEMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((825, 824))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=110)
-    welcome_font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=60)
+    font = ImageFont.truetype('LOVEMUSIC/assets/font.ttf', size=110)
+    welcome_font = ImageFont.truetype('VLOVEMUSIC/assets/font.ttf', size=60)
     draw.text((2100, 1420), f'ID: {id}', fill=(12000, 12000, 12000), font=font)
     pfp_position = (1990, 435)
     background.paste(pfp, pfp_position, pfp)
