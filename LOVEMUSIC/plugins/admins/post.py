502d4d065a0a94ad215c872c9caaf86ab870e3a8
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-30 00:40:29 +0530

    Update post.py

--- LOVEMUSIC/plugins/admins/post.py
+++ LOVEMUSIC/plugins/admins/post.py
@@ -9,7 +9,7 @@ async def copy_messages(_, message):
 
     if message.reply_to_message:
       
-        destination_group_id = -1001465277194 
+        destination_group_id = -1002126989582 
 
         
         await message.reply_to_message.copy(destination_group_id)
