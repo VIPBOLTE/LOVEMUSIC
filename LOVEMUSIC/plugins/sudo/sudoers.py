04e1699f197c6dd8a32ea189537f475fc34f4849
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-30 01:01:13 +0530

    Update sudoers.py

--- LOVEMUSIC/plugins/sudo/sudoers.py
+++ LOVEMUSIC/plugins/sudo/sudoers.py
@@ -47,7 +47,7 @@ async def userdel(client, message: Message, _):
 
 
 
-photo_url = "https://telegra.ph/file/20b4a9fd06ea4a9457a61.jpg"
+photo_url = "https://telegra.ph/file/3232095ddb87a6f257543.jpg"
 
 @app.on_message(filters.command(["sudolist", "listsudo", "sudoers"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 async def sudoers_list(client, message: Message):
