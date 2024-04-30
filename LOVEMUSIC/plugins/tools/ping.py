3da433021284f259aae1d2839e1facf3747d89a3
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-30 13:28:33 +0530

    Update ping.py

--- LOVEMUSIC/plugins/tools/ping.py
+++ LOVEMUSIC/plugins/tools/ping.py
@@ -102,10 +102,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/TG_FRIENDSS",
+                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/goku_groups",
             ),
             InlineKeyboardButton(
-                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/VIP_CREATORS",
+                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/channelz_k",
             )
         ],
         [
