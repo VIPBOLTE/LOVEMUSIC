c62f27fa01e0268fc1eeaa7b0773f98c7ecb9500
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-23 19:10:40 +0530

    Update play.py

--- LOVEMUSIC/plugins/play/play.py
+++ LOVEMUSIC/plugins/play/play.py
@@ -779,7 +779,7 @@ from LOVEMUSIC.utils.inline import (
     stream_markup2,
     panel_markup_4,
 )
-from LOVEMUSIC.utils.pastebin import VIPBin
+from LOVEMUSIC.utils.pastebin import LOVEBin
 from LOVEMUSIC.utils.stream.queue import put_queue, put_queue_index
 from youtubesearchpython.__future__ import VideosSearch
 
