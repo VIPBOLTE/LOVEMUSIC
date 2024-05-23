15a0b43ba3da6f21c064eb61ff58c75597922651
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-23 17:40:38 +0530

    Update play.py

--- LOVEMUSIC/plugins/play/play.py
+++ LOVEMUSIC/plugins/play/play.py
@@ -1053,7 +1053,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await VIP.join_call(chat_id, original_chat_id, file_path, video=status)
+            await LOVE.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -1109,7 +1109,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await VIP.join_call(
+            await LOVE.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -1167,7 +1167,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await VIP.join_call(
+            await LOVE.join_call(
                 chat_id,
                 original_chat_id,
                 link,
