284bab6007cd212eb968c8741da6f0dc7deb2069
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-26 22:02:11 +0530

    Update zowner.py

--- LOVEMUSIC/plugins/tools/zowner.py
+++ LOVEMUSIC/plugins/tools/zowner.py
@@ -5,17 +5,17 @@ import os
 import re
 import asyncio
 import time
-from LOVEMUSIC import app
-from LOVEMUSIC.utils.database import add_served_chat, delete_served_chat
+from VIPMUSIC import app
+from VIPMUSIC.utils.database import add_served_chat, delete_served_chat
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from LOVEMUSIC.utils.database import get_assistant
+from VIPMUSIC.utils.database import get_assistant
 import asyncio
-from LOVEMUSIC.misc import SUDOERS
-from LOVEMUSIC.mongo.afkdb import HEHE
-from LOVEMUSIC.core.userbot import Userbot
+from VIPMUSIC.misc import SUDOERS
+from VIPMUSIC.mongo.afkdb import LOGGERS as OWNERS
+from VIPMUSIC.core.userbot import Userbot
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
-from LOVEMUSIC import app
+from VIPMUSIC import app
 import asyncio
 import random
 from pyrogram import Client, filters
@@ -26,66 +26,56 @@ from pyrogram.errors import (
     UserAlreadyParticipant,
     UserNotParticipant,
 )
-from LOVEMUSIC import app
-from LOVEMUSIC.utils.LOVE_ban import admin_filter
-from LOVEMUSIC.utils.decorators.userbotjoin import UserbotWrapper
-from LOVEMUSIC.utils.database import get_assistant, is_active_chat
+from VIPMUSIC import app
+from VIPMUSIC.utils.vip_ban import admin_filter
+from VIPMUSIC.utils.decorators.userbotjoin import UserbotWrapper
+from VIPMUSIC.utils.database import get_assistant, is_active_chat
 
-@app.on_message(
-    filters.command("repo")
-    & filters.group)
+
+@app.on_message(filters.command("repo"))
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/a0c824b3ad40e8bd86db7.jpg",
+        photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
         caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/DAXXTEAM/DAXXMUSIC")
+                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
+                    )
                 ]
             ]
         ),
     )
 
-@app.on_message(
-    filters.command("repo")
-    & filters.group)
-async def help(client: Client, message: Message):
-    userbot = await get_assistant(chat_id)
+
+@app.on_message(filters.command("clone"))
+async def clones(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/a0c824b3ad40e8bd86db7.jpg",
-        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
+        caption=f"""**🙂You Are Not Sudo User So You Are Not Allowed To Clone Me.**\n**😌Click Given Below Button And Host Manually Otherwise Contact Owner Or Sudo Users For Clone.**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/DAXXTEAM/DAXXMUSIC")
+                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
+                    )
                 ]
             ]
         ),
     )
 
-@app.on_message(
-    filters.command("repo")
-    & filters.private)
-async def help(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://telegra.ph/file/a0c824b3ad40e8bd86db7.jpg",
-        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/DAXXTEAM/DAXXMUSIC")
-                ]
-            ]
-        ),
-    )
 
 # --------------------------------------------------------------------------------- #
 
-@app.on_message(filters.command(["hi", "hii", "hello", "hui", "good", "gm", "ok", "bye", "welcome", "thanks"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group)
+
+@app.on_message(
+    filters.command(
+        ["hi", "hii", "hello", "hui", "good", "gm", "ok", "bye", "welcome", "thanks"],
+        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
+    )
+    & filters.group
+)
 async def bot_check(_, message):
     chat_id = message.chat.id
     await add_served_chat(chat_id)
@@ -94,18 +84,19 @@ async def bot_check(_, message):
 # --------------------------------------------------------------------------------- #
 
 
-
-
 import asyncio
 import time
 
-@app.on_message(filters.command("gadd") & filters.user(int(HEHE)))
-async def add_all(client, message):
+
+@app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
+async def add_allbot(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
-        await message.reply("**⚠️ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @SIESTA_MAX_PROBOT`**")
+        await message.reply(
+            "**⚠️ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @TG_VC_BOT`**"
+        )
         return
-    
+
     bot_username = command_parts[1]
     try:
         userbot = await get_assistant(message.chat.id)
@@ -114,11 +105,12 @@ async def add_all(client, message):
         done = 0
         failed = 0
         lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
-        
+        await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002126989582:
+            if dialog.chat.id == -1002120144597:
                 continue
             try:
+
                 await userbot.add_chat_members(dialog.chat.id, app_id)
                 done += 1
                 await lol.edit(
@@ -130,7 +122,7 @@ async def add_all(client, message):
                     f"**🔂 ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅɪɴɢ ʙʏ»** @{userbot.username}"
                 )
             await asyncio.sleep(3)  # Adjust sleep time based on rate limits
-        
+
         await lol.edit(
             f"**➻ {bot_username} ʙᴏᴛ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ🎉**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
         )
