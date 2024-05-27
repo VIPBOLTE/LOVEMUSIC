c623e2c8463489e91c394ef643daba04ff8733ab
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-27 14:36:52 +0530

    Update settings.py

--- LOVEMUSIC/cplugin/settings.py
+++ LOVEMUSIC/cplugin/settings.py
@@ -8,8 +8,8 @@ from pyrogram.types import (
     Message,
 )
 
-from VIPMUSIC import app
-from VIPMUSIC.utils.database import (
+from LOVEMUSIC import app
+from LOVEMUSIC.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,15 +25,15 @@ from VIPMUSIC.utils.database import (
     skip_off,
     skip_on,
 )
-from VIPMUSIC.utils.decorators.admins import ActualAdminCB
-from VIPMUSIC.utils.decorators.language import language, languageCB
-from VIPMUSIC.utils.inline.settings import (
+from LOVEMUSIC.utils.decorators.admins import ActualAdminCB
+from LOVEMUSIC.utils.decorators.language import language, languageCB
+from LOVEMUSIC.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from VIPMUSIC.utils.inline.start import private_panel
+from LOVEMUSIC.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
