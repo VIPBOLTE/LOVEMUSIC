ccbd491292f0d322da4c33d48140c31255f6e2f5
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-27 19:52:26 +0530

    Update reload.py

--- LOVEMUSIC/plugins/tools/reload.py
+++ LOVEMUSIC/plugins/tools/reload.py
@@ -99,7 +99,7 @@ async def restartbot(client, message: Message, _):
             pass
         try:
             db[chat_id] = []
-            await ❤.stop_stream_force(chat_id)
+            await.stop_stream_force(chat_id)
         except:
             pass
     return await mystic.edit_text(_["reload_5"].format(app.mention))
