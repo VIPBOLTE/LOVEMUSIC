ed8346e432f10b43024e6a0b8cecb9ef663a141f
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-27 09:27:24 +0530

    Update broadcast.py

--- LOVEMUSIC/plugins/misc/broadcast.py
+++ LOVEMUSIC/plugins/misc/broadcast.py
@@ -117,17 +117,17 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_5"])
         text = _["broad_6"]
-        from LOVEMUSIC.core.userbot import assistants
+        from VIPMUSIC.core.userbot import assistants
 
         for num in assistants:
             sent = 0
             client = await get_client(num)
             async for dialog in client.get_dialogs():
                 try:
-                    await client.forward_messages(
-                        dialog.chat.id, y, x
-                    ) if message.reply_to_message else await client.send_message(
-                        dialog.chat.id, text=query
+                    (
+                        await client.forward_messages(dialog.chat.id, y, x)
+                        if message.reply_to_message
+                        else await client.send_message(dialog.chat.id, text=query)
                     )
                     sent += 1
                     await asyncio.sleep(3)
