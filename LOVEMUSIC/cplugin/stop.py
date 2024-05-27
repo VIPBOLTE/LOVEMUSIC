3a8622c8d3b4b2efd318d0cdafba9d4d512c2b21
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-27 13:50:32 +0530

    Update stop.py

--- LOVEMUSIC/cplugin/stop.py
+++ LOVEMUSIC/cplugin/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from VIPMUSIC import app
-from VIPMUSIC.core.call import VIP
-from VIPMUSIC.utils.database import set_loop
-from VIPMUSIC.utils.decorators import AdminRightsCheck
-from VIPMUSIC.utils.inline import close_markup
+from LOVEMUSIC import app
+from LOVEMUSIC.core.call import LOVE
+from LOVEMUSIC.utils.database import set_loop
+from LOVEMUSIC.utils.decorators import AdminRightsCheck
+from LOVEMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -21,7 +21,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await VIP.stop_stream(chat_id)
+    await LOVE.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
