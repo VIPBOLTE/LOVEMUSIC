ef2e94880dc3b67d12c764707a81430bbcedd71b
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-27 15:54:45 +0530

    Update inline.py

--- LOVEMUSIC/cplugin/inline.py
+++ LOVEMUSIC/cplugin/inline.py
@@ -5,8 +5,8 @@ from pyrogram.types import (
 )
 from youtubesearchpython.__future__ import VideosSearch
 from pyrogram import filters, Client
-from VIPMUSIC import app
-from VIPMUSIC.utils.inlinequery import answer
+from LOVEMUSIC import app
+from LOVEMUSIC.utils.inlinequery import answer
 from config import BANNED_USERS
 
 
