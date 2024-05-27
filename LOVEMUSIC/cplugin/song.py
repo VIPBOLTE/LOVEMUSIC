db424f86987813459d336cf9e43707e2af7eede4
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-27 13:57:56 +0530

    Update song.py

--- LOVEMUSIC/cplugin/song.py
+++ LOVEMUSIC/cplugin/song.py
@@ -16,7 +16,7 @@ from youtubesearchpython import VideosSearch
 from youtubesearchpython import SearchVideos
 from time import time
 import asyncio
-from VIPMUSIC.utils.extraction import extract_user
+from LOVEMUSIC.utils.extraction import extract_user
 
 # Define a dictionary to track the last message timestamp for each user
 user_last_message_time = {}
