c3aa46f2d19c36f7eaa420d20e4d56edbc44a305
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-27 16:19:06 +0530

    Update active.py

--- LOVEMUSIC/cplugin/active.py
+++ LOVEMUSIC/cplugin/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from unidecode import unidecode
 
-from VIPMUSIC import app
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import (
+from LOVEMUSIC import app
+from LOVEMUSIC.misc import SUDOERS
+from LOVEMUSIC.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
