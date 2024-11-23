610f72cca2ff82fd1ba8c0d09fcfef88e6c90b28
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-23 22:49:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ load_dotenv()
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN","7116275578:AAHPONS1LMgJoQqc4a8zisEVjH3rH8lDjlQ")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://GOKU:MISSBHOPALI@goku.pzzsl8d.mongodb.net/?retryWrites=true&w=majority")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
