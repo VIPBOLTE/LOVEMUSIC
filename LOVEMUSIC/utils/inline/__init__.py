1a52a3797abed918dc78877897b4e8b169b36a7a
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-24 18:52:17 +0530

    Update __init__.py

--- LOVEMUSIC/utils/inline/__init__.py
+++ LOVEMUSIC/utils/inline/__init__.py
@@ -1,9 +1,8 @@
-from .extras import *
 from .help import *
 from .play import *
+from .playlist import *
 from .queue import *
 from .settings import *
+from .song import *
 from .speed import *
-from .start import *
-from .sudolist import *
-from .playlist import *
+from .start import
