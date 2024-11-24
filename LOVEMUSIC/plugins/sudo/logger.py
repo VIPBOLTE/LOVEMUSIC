ddd326dd7b761acc9f6a54bed4c081be712a316f
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-25 00:14:02 +0530

    Update logger.py

--- LOVEMUSIC/plugins/sudo/logger.py
+++ LOVEMUSIC/plugins/sudo/logger.py
@@ -1,23 +1,29 @@
 from pyrogram import filters
 
+import config
+from strings import get_command
 from LOVEMUSIC import app
 from LOVEMUSIC.misc import SUDOERS
 from LOVEMUSIC.utils.database import add_off, add_on
 from LOVEMUSIC.utils.decorators.language import language
 
+# Commands
+LOGGER_COMMAND = get_command("LOGGER_COMMAND")
 
-@app.on_message(filters.command(["logger"]) & SUDOERS)
+
+@app.on_message(filters.command(LOGGER_COMMAND) & SUDOERS)
 @language
 async def logger(client, message, _):
     usage = _["log_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip().lower()
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
     if state == "enable":
-        await add_on(2)
+        await add_on(config.LOG)
         await message.reply_text(_["log_2"])
     elif state == "disable":
-        await add_off(2)
+        await add_off(config.LOG)
         await message.reply_text(_["log_3"])
     else:
         await message.reply_text(usage)
