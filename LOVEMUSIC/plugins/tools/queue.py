08eff7a24c0376d37c971d73e2c716e9c3b5e95d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-24 21:50:24 +0530

    Update queue.py

--- LOVEMUSIC/plugins/tools/queue.py
+++ LOVEMUSIC/plugins/tools/queue.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import asyncio
 
 from pyrogram import filters
