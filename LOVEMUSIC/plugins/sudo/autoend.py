0db752f9923e21df6a269b1a409ee4528f970f54
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-25 00:33:25 +0530

    Update autoend.py

--- LOVEMUSIC/plugins/sudo/autoend.py
+++ LOVEMUSIC/plugins/sudo/autoend.py
@@ -1,24 +1,29 @@
-from pyrogram import filters
-from pyrogram.types import Message
 
-from LOVEMUSIC import app
+from pyrogram import filters
+
+from strings import get_command
+from VIPMUSIC import app
 from LOVEMUSIC.misc import SUDOERS
 from LOVEMUSIC.utils.database import autoend_off, autoend_on
 
+# Commands
+AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
 
-@app.on_message(filters.command("autoend") & SUDOERS)
-async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+
+@app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
+async def auto_end_stream(client, message):
+    usage = "**ᴜsᴀɢᴇ:**\n\n/autoend [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip().lower()
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "Aᴜᴛᴏ Eɴᴅ Sᴛʀᴇᴀᴍ Eɴᴀʙʟᴇᴅ.\n\nBᴏᴛ ᴡɪʟʟ ʟᴇᴀᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀғᴛᴇʀ 3 ᴍɪɴs ɪғ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ ᴡɪᴛʜ ᴀ ᴡᴀʀɴɪɴɢ ᴍᴇssᴀɢᴇ.."
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+        await message.reply_text("ᴀᴜᴛᴏᴇɴᴅ ᴅɪsᴀʙʟᴇᴅ")
     else:
         await message.reply_text(usage)
