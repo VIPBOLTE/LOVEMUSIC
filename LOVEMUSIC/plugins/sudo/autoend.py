362c26015e375bef88366a891d4d869080cf02d9
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-25 00:33:45 +0530

    Update autoend.py

--- LOVEMUSIC/plugins/sudo/autoend.py
+++ LOVEMUSIC/plugins/sudo/autoend.py
@@ -2,7 +2,7 @@
 from pyrogram import filters
 
 from strings import get_command
-from VIPMUSIC import app
+from LOVEMUSIC import app
 from LOVEMUSIC.misc import SUDOERS
 from LOVEMUSIC.utils.database import autoend_off, autoend_on
 
