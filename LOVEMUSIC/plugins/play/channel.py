49064f8a087d66109b22ba87d63018f815331d1d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-25 00:58:53 +0530

    Update channel.py

--- LOVEMUSIC/plugins/play/channel.py
+++ LOVEMUSIC/plugins/play/channel.py
@@ -2,53 +2,27 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
+from config import BANNED_USERS
+from strings import get_command
 from LOVEMUSIC import app
 from LOVEMUSIC.utils.database import set_cmode
 from LOVEMUSIC.utils.decorators.admins import AdminActual
-from config import BANNED_USERS
-from time import time
-import asyncio
-from LOVEMUSIC.utils.extraction import extract_user
 
-# Define a dictionary to track the last message timestamp for each user
-user_last_message_time = {}
-user_command_count = {}
-# Define the threshold for command spamming (e.g., 20 commands within 60 seconds)
-SPAM_THRESHOLD = 2
-SPAM_WINDOW_SECONDS = 5
+### Multi-Lang Commands
+CHANNELPLAY_COMMAND = get_command("CHANNELPLAY_COMMAND")
 
 
-@app.on_message(filters.command(["channelplay"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(CHANNELPLAY_COMMAND) & filters.group & ~BANNED_USERS)
 @AdminActual
 async def playmode_(client, message: Message, _):
-    user_id = message.from_user.id
-    current_time = time()
-    # Update the last message timestamp for the user
-    last_message_time = user_last_message_time.get(user_id, 0)
-
-    if current_time - last_message_time < SPAM_WINDOW_SECONDS:
-        # If less than the spam window time has passed since the last message
-        user_last_message_time[user_id] = current_time
-        user_command_count[user_id] = user_command_count.get(user_id, 0) + 1
-        if user_command_count[user_id] > SPAM_THRESHOLD:
-            # Block the user if they exceed the threshold
-            hu = await message.reply_text(
-                f"**{message.from_user.mention} ᴘʟᴇᴀsᴇ ᴅᴏɴᴛ ᴅᴏ sᴘᴀᴍ, ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 5 sᴇᴄ**"
-            )
-            await asyncio.sleep(3)
-            await hu.delete()
-            return
-    else:
-        # If more than the spam window time has passed, reset the command count and update the message timestamp
-        user_command_count[user_id] = 1
-        user_last_message_time[user_id] = current_time
-
     if len(message.command) < 2:
-        return await message.reply_text(_["cplay_1"].format(message.chat.title))
+        return await message.reply_text(
+            _["cplay_1"].format(message.chat.title, CHANNELPLAY_COMMAND[0])
+        )
     query = message.text.split(None, 2)[1].lower().strip()
     if (str(query)).lower() == "disable":
         await set_cmode(message.chat.id, None)
-        return await message.reply_text(_["cplay_7"])
+        return await message.reply_text("Channel Play Disabled")
     elif str(query) == "linked":
         chat = await app.get_chat(message.chat.id)
         if chat.linked_chat:
@@ -67,15 +41,18 @@ async def playmode_(client, message: Message, _):
         if chat.type != ChatType.CHANNEL:
             return await message.reply_text(_["cplay_5"])
         try:
-            async for user in app.get_chat_members(
+            admins = app.get_chat_members(
                 chat.id, filter=ChatMembersFilter.ADMINISTRATORS
-            ):
-                if user.status == ChatMemberStatus.OWNER:
-                    cusn = user.user.username
-                    crid = user.user.id
+            )
         except:
             return await message.reply_text(_["cplay_4"])
-        if crid != message.from_user.id:
-            return await message.reply_text(_["cplay_6"].format(chat.title, cusn))
+        async for users in admins:
+            if users.status == ChatMemberStatus.OWNER:
+                creatorusername = users.user.username
+                creatorid = users.user.id
+        if creatorid != message.from_user.id:
+            return await message.reply_text(
+                _["cplay_6"].format(chat.title, creatorusername)
+            )
         await set_cmode(message.chat.id, chat.id)
         return await message.reply_text(_["cplay_3"].format(chat.title, chat.id))
