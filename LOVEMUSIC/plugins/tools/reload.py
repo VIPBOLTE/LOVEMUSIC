e64fc58be5691ecb5f800371040e8f8c2f2bdc08
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-24 21:45:03 +0530

    Update reload.py

--- LOVEMUSIC/plugins/tools/reload.py
+++ LOVEMUSIC/plugins/tools/reload.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
 import asyncio
 import logging
