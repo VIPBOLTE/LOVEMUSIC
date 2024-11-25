29017a2080a1af9623269e189dfef3af26f29f7f
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-25 08:06:25 +0530

    Update skip.py

--- LOVEMUSIC/plugins/admins/skip.py
+++ LOVEMUSIC/plugins/admins/skip.py
@@ -5,7 +5,7 @@ import config
 from config import BANNED_USERS
 from strings import get_command
 from LOVEMUSIC import YouTube, app
-from LOVEMUSIC.core.call import VIP
+from LOVEMUSIC.core.call import LOVE
 from LOVEMUSIC.misc import db
 from LOVEMUSIC.utils.database import get_loop
 from LOVEMUSIC.utils.decorators import AdminRightsCheck
