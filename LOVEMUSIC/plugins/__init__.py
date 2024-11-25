64cbfc06147f462f15eeb2515d8142155c707abc
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-11-25 10:36:33 +0530

    Update __init__.py

--- LOVEMUSIC/plugins/__init__.py
+++ LOVEMUSIC/plugins/__init__.py
@@ -8,7 +8,7 @@ import sys
 from os.path import abspath, dirname, isfile, join
 
 from config import EXTRA_PLUGINS, EXTRA_PLUGINS_FOLDER, EXTRA_PLUGINS_REPO
-from VIPMUSIC import LOGGER
+from LOVEMUSIC import LOGGER
 
 logger = LOGGER(__name__)
 
