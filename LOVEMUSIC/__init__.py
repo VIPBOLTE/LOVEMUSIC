e86e7173be511620304d8d7ccff681da89ae8d8f
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-03-10 02:15:31 +0530

    Create __init__.py

---
+++ LOVEMUSIC/__init__.py
@@ -0,0 +1,26 @@
+from LOVEMUSIC.core.bot import LOVEMUSIC
+from LOVEMUSIC.core.dir import dirr
+from LOVEMUSIC.core.git import git
+from LOVEMUSIC.core.userbot import Userbot
+from LOVEMUSIC.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = LOVEMUSIC()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
