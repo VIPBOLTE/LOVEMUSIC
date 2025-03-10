28877f1a808216c11aec43482fe0c4f9c08f2ad4
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-03-10 23:44:44 +0530

    Update thumbnails.py

--- LOVEMUSIC/utils/thumbnails.py
+++ LOVEMUSIC/utils/thumbnails.py
@@ -71,8 +71,8 @@ async def get_thumb(videoid):
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.5)
         draw = ImageDraw.Draw(background)
-        arial = ImageFont.truetype("ChampuXMusic/assets/font2.ttf", 30)
-        font = ImageFont.truetype("ChampuXMusic/assets/font.ttf", 30)
+        arial = ImageFont.truetype("LOVEMUSIC/assets/font2.ttf", 30)
+        font = ImageFont.truetype("LOVEMUSIC/assets/font.ttf", 30)
         draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
         draw.text(
             (55, 560),
