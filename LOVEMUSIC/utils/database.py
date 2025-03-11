000a7e3f1d8ffd64a316bfee30917bd8e1cd6dd9
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-03-11 11:58:35 +0530

    Update database.py

--- LOVEMUSIC/utils/database.py
+++ LOVEMUSIC/utils/database.py
@@ -710,3 +710,32 @@ async def commanddelete_on(chat_id: int):
     if chat_id in command:
         command.remove(chat_id)
         save_command()
+
+
+
+async def get_private_served_chats() -> list:
+    chats_list = []
+    async for chat in privatedb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def is_served_private_chat(chat_id: int) -> bool:
+    chat = await privatedb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def add_private_chat(chat_id: int):
+    is_served = await is_served_private_chat(chat_id)
+    if is_served:
+        return
+    return await privatedb.insert_one({"chat_id": chat_id})
+
+
+async def remove_private_chat(chat_id: int):
+    is_served = await is_served_private_chat(chat_id)
+    if not is_served:
+        return
+    return await privatedb.delete_one({"chat_id": chat_id})
